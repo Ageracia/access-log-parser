db997eae4ee412b8ab920085cd227b09f879a0e7
Dmitry Monakhov <dmonakhov1502@gmail.com>
2024-12-16 21:27:10 +0300

    Обновление класса main: класс выполняет простые математические операции с двумя числами

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System. in).nextLine();
-        System. out.println("Длина текста: " + text.length ());
+        System.out.println ("Введите первое число:") ;
+        int num_1 = new Scanner(System.in).nextInt() ;
+        System.out.println ("Введите второе число:") ;
+        int num_2 = new Scanner(System.in).nextInt() ;
+        int sum = num_1 + num_2;
+        int difference = num_1 - num_2;
+        int multiplication = num_1 * num_2;
+        double quotient = (double) num_1 /num_2;
+        System.out.println(
+                "Сумма чисел равна " + sum + "\n" +
+                "Разность чисел равна " + difference + "\n" +
+                "Произведение чисел равно " + multiplication + "\n" +
+                "Частное чисел равно " + quotient
+        );
     }
 }
