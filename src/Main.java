4c1bf95955a7db96e6d0b9e844ce43bc327446da
Dmitry Monakhov <dmonakhov1502@gmail.com>
2025-04-14 08:30:01 +0300

    Добавлен счетчик запросов от YandexBot и Googlebot

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,20 +24,33 @@ public class Main {
             }
             count++;
             System.out.println("Путь указан верно!\nЭто файл номер " + count);
-
+            int googleCounter = 0;
+            int yandexCounter = 0;
             int linesCounter = 0;
-            int maxLength = 0;
-            int minLength = Integer.MAX_VALUE;
             try{
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader =
                         new BufferedReader(fileReader);
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    linesCounter+=1;
+                    String agent = "";
+                    Matcher matcher = Pattern.compile("\\((.*)\\)").matcher(line);
+                    if (matcher.find()) {
+                        String firstBrackets = matcher.group(1);
+                        String[] parts = firstBrackets.split(";");
+                        linesCounter += 1;
+                        for (int i = 0; i < parts.length; i++) {
+                            parts[i] = parts[i].replace(" ", "");
+                        }
+                        if (parts.length >= 2) {
+                            String fragment = parts[1];
+                            int index = fragment.indexOf("/");
+                            if (index != -1) agent = fragment.substring(0, index);
+                        }
+                    }
+                    if (agent.equals("YandexBot")) yandexCounter+=1;
+                    if (agent.equals("Googlebot")) googleCounter+=1;
                     int length = line.length();
-                    if (length > maxLength) maxLength = length;
-                    if (length < minLength) minLength = length;
                     if (length > 1024){
                         throw new TooLongStringException("Длина строки больше 1024 символов!");
                     }
@@ -43,10 +58,12 @@ public class Main {
             } catch(Exception ex) {
                 ex.printStackTrace();
             }
-            System.out.println("Количество строк: " + linesCounter);
-            System.out.println("Длина самой длинной строки: " + maxLength);
-            System.out.println("Длина самой короткой строки " + minLength);
+            System.out.println("Доля YandexBot от общего числа: " + proportion(yandexCounter,linesCounter) + "%");
+            System.out.println("Доля GoogleBot от общего числа: " + proportion(googleCounter,linesCounter) + "%");
         }
+    }
+    public static double proportion(int bot, int lines){
+        return (double) bot/lines * 100;
     }
 
 
